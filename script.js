10c1a1cc8c0a40f3f62870286e52b8d95ead32ba
nrdj <nriedeldj@gmail.com>
2022-01-29 11:10:41 -0300

    add new task to dom after clicking add button

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 $(window).on("load", function () {
 
-  var addTasksToDom = function (allTasksArr) {
-      allTasksArr.forEach((element, i) => {
-        var newCol = $(`
-        <div class="col-1"></div>
-        <div class="col-9">
-          <div class="form-check d-inline-block">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <label class="form-check-label" for="flexCheckDefault">
-            </label>
-          </div>
-          <p class='d-inline-block'>${allTasksArr[i].content}</p>
+  var addTaskToDom = function (task) {
+      var newCol = $(`
+      <div class="col-1"></div>
+      <div class="col-9">
+        <div class="form-check d-inline-block">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">
+          </label>
         </div>
-        <div class="col-2">
-          <button type="button" class="btn btn-danger btn-sm">X</button>
-        </div>`);
-        
-        $(newCol).appendTo(`.row`);
-      });
+        <p class='d-inline-block'>${task}</p>
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn btn-danger btn-sm">X</button>
+      </div>`);
+      
+      $(newCol).appendTo(`.row`);
   }
 
+  // var addTasksToDom = function (allTasksArr) {
+  //     allTasksArr.forEach((element, i) => {
+  //       var newCol = $(`
+  //       <div class="col-1"></div>
+  //       <div class="col-9">
+  //         <div class="form-check d-inline-block">
+  //           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  //           <label class="form-check-label" for="flexCheckDefault">
+  //           </label>
+  //         </div>
+  //         <p class='d-inline-block'>${allTasksArr[i].content}</p>
+  //       </div>
+  //       <div class="col-2">
+  //         <button type="button" class="btn btn-danger btn-sm">X</button>
+  //       </div>`);
+        
+  //       $(newCol).appendTo(`.row`);
+  //     });
+  // }
 
   var importTasks = function () {
     $.ajax({
@@ -32,7 +49,12 @@ $(window).on("load", function () {
       success: function (response, textStatus) {
         var allTasksArr = response.tasks;
         console.log(allTasksArr);
-        addTasksToDom(allTasksArr);
+
+        allTasksArr.forEach((element, i) => {
+          var task = allTasksArr[i].content
+          addTaskToDom(task);
+        });
+
       },
 
       error: function (request, textStatus, errorMessage) {
@@ -41,12 +63,12 @@ $(window).on("load", function () {
     });
   };
 
-  var addTaskToList = function (newTask) {};
+//api key 224//
 
   var sendTaskToApi = function (newTask) {
     $.ajax({
       type: "POST",
-      url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=224",
+      url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1",
       contentType: "application/json",
       dataType: "json",
       data: JSON.stringify({
@@ -64,13 +86,20 @@ $(window).on("load", function () {
   };
 
   var addTask = function () {
-    var newTask = document.querySelector("input").value;
-    sendTaskToApi(newTask);
-    addTaskToList(newTask);
+    var newTask = document.querySelector("input").value
+
+    if (newTask) {
+      console.log(newTask)
+      sendTaskToApi(newTask);
+      addTaskToDom(newTask);
+    } else {
+      console.log('empty')
+    }
+
   };
 
   $(".btn-add").click(function () {
-    console.log("Button Clicked");
+    console.log("btn clicked");
     addTask();
   });
 
